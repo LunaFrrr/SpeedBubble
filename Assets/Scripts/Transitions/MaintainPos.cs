6a70a021aec9d3fd3461b98b57b52731e63ac4d1
Cotawh <92425971+Cotawh@users.noreply.github.com>
2025-03-12 15:17:53 +0100

    transiciones arreglo
    se intentó arreglar la vibracion de los puntitos, pero al final se mueven junto con la cortinilla. derrota para los fans de animal crossing

--- Assets/Scripts/Transitions/MaintainPos.cs
+++ Assets/Scripts/Transitions/MaintainPos.cs
@@ -3,35 +3,21 @@ using UnityEngine;
 public class MaintainPos : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    Vector3 posOriginal;
+    Vector3 posBase;
     Vector3 offSet;
     Vector3 newPos;
     public GameObject parent;
     void Start()
     {
-        posOriginal = new Vector3(-1000,-400,0);
+        posBase = new Vector3(-3000,-1000,0);
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        offSet = posOriginal + parent.transform.position;
-
-        if (parent.transform.position.x < 0)
-        {
-            newPos = new Vector3(-offSet.x, -offSet.y, 0f);
-            transform.position = parent.transform.position + newPos;
-        }
-        else
-        {
-            newPos = new Vector3(offSet.x, offSet.y, 0f);
-            transform.position = parent.transform.position - newPos;
-        }
-
-
-
+        offSet = parent.transform.position - posBase;
+        
+        transform.localPosition = new Vector3(3000 , 1000, 0) - offSet;
     }
 }
